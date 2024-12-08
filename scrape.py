cb0a988b14bbc8c614315c0c3183b2977462eb24
Eby Elanjikal <86768805+eby0303@users.noreply.github.com>
2024-12-08 10:38:10 +0000

    chnages

---
+++ scrape.py
@@ -0,0 +1,7 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from autoscraper import AutoScraper
+
