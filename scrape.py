bfa3b75f80af20801c6368a6bf39e26115a46632
Eby Elanjikal <86768805+eby0303@users.noreply.github.com>
2024-12-11 16:01:37 +0000

    deleted scrape

--- scrape.py
+++
@@ -1,7 +0,0 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from autoscraper import AutoScraper
-
